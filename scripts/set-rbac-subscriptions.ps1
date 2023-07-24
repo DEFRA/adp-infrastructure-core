ae7f39a397aede04f11d0c90a0d6410b926a69b0
ganeshg <ganesh.b.ghadge@capgemini.com>
2023-07-24 16:27:48 +0100

    tier2ApplicationClientIdSecretName debug

--- scripts/set-rbac-subscriptions.ps1
+++ scripts/set-rbac-subscriptions.ps1
@@ -4,7 +4,11 @@ param(
     [string][Parameter(mandatory=$true)] $Tier2ApplicationClientIdSecretName
 )
 
-$tier2ApplicationClientId = (Get-AzKeyVaultSecret -VaultName $KeyVaultName -Name $Tier2ApplicationClientIdSecretName).SecretValueText
+Write-Host "Param:KeyVaultName =  $($KeyVaultName)"
+Write-Host "Param:Tier2ApplicationClientIdSecretName =  $($Tier2ApplicationClientIdSecretName)"
+
+$tier2ApplicationClientId = Get-AzKeyVaultSecret -VaultName $KeyVaultName -Name $Tier2ApplicationClientIdSecretName -AsPlainText
+
 Write-Host "Tier2ApplicationClientId value is =  $($tier2ApplicationClientId)"
 
 Write-Host "Assigning $($tier2ApplicationClientId) User Acess Administrator role to Subscription $($SubscriptionName)"
