df93221b79d7e8495713369bb358644ea36605f8
ganeshg <ganesh.b.ghadge@capgemini.com>
2023-08-02 17:44:34 +0100

    fix

--- scripts/Setup-Service-Connection.ps1
+++ scripts/Setup-Service-Connection.ps1
@@ -85,7 +85,7 @@ try {
     Write-Debug "spClientId=$spClientId"
 
     Write-Host "Fetching Keyvault secret $($kvClientPasswordSecretName) from $($KeyVaultName)"
-    $spClientPassword = az keyvault secret show --name $kvClientPasswordSecretName --vault-name $kvName --query "value" -o tsv
+    $spClientPassword = az keyvault secret show --name $kvClientPasswordSecretName --vault-name $KeyVaultName --query "value" -o tsv
 
     # Set PAT as an environment variable for DevOps Login
     # $env:AZURE_DEVOPS_EXT_PAT = $env:SYSTEM_ACCESSTOKEN
@@ -97,6 +97,7 @@ try {
 
     Write-Debug "Check if $($ServiceEndpointName) exists"
     $isServiceEndpointAlreadyExist = az devops service-endpoint list --query "[?name=='$ServiceEndpointName'].name" -o tsv
+    Write-Debug "isServiceEndpointAlreadyExist=$isServiceEndpointAlreadyExist"
 
     if (-not $isServiceEndpointAlreadyExist) {
         Write-Host "Creating ServiceEndpointName $($ServiceEndpointName).."
