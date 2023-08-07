c055a5260fefb92aac7531dd3973caba3f55e385
ganeshg <ganesh.b.ghadge@capgemini.com>
2023-08-07 17:12:35 +0100

    Service endpoint create update verify operations with advance functions

---
+++ scripts/service-connection/utilities/Get-DefaultHeadersWithAccessToken.ps1
@@ -0,0 +1,41 @@
+<#
+.SYNOPSIS
+Initialize default headers With AccessToken.
+
+.DESCRIPTION
+Initialize default headers With AccessToken require to perform devops rest api call.
+
+.PARAMETER PatToken
+Optional. Pat Token with Service endpoint manage permission. 
+If PatToken is not provided $env:SYSTEM_ACCESSTOKEN will be used. Make sure Project build service identity has granted 
+required permissions to perform create or update service endpoint operatins.
+For e.g. To create service connection in Defra-FFC project 'DEFRA-FFC Build Service (defragovuk)' identity should be granted 'Administrator' permissions at Service connection scope.
+
+.EXAMPLE
+.\Get-DefaultHeadersWithAccessToken
+#> 
+Function Get-DefaultHeadersWithAccessToken() {
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory = $false)]
+        [ValidateNotNullOrEmpty()]
+        [string]$PatToken
+    )
+
+    [string]$functionName = $MyInvocation.MyCommand    
+    Write-Debug "${functionName}:Entered"
+    
+    $accessTokenHeaders = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+    $accessTokenHeaders.Add("Content-Type", "application/json")
+    
+    if([string]::IsNullOrWhiteSpace($PatToken)) {
+        $accessTokenHeaders.Add("Authorization", "Bearer $env:SYSTEM_ACCESSTOKEN")
+    }
+    else {
+        $token = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes(":$($PatToken)"))
+        $accessTokenHeaders.Add("Authorization", "Basic $token")
+    }
+    
+    Write-Debug "${functionName}:Exited"
+    return $accessTokenHeaders
+}
---
+++ scripts/service-connection/utilities/Test-ServiceEndpoint.ps1
@@ -0,0 +1,63 @@
+<#
+.SYNOPSIS
+Verify service endpoint (ServiceConnection).
+
+.DESCRIPTION
+Verify service endpoint (ServiceConnection) is setup correctly using endpointproxy post api. This api returns 200(OK) response if configuration (for e.g. ServicePrincipal key or clientID) is 
+correct or else returns 400.
+
+.PARAMETER ServiceEndpointId
+Mandatory. Service Endpoint Id.
+
+.PARAMETER ServiceEndpointRequestBody
+Mandatory. Service Endpoint Request Body
+
+.PARAMETER ProjectName
+Mandatory. Azure devops project name
+
+.PARAMETER OrgnizationUri
+Mandatory. Azure devops project Orgnization Uri
+
+.EXAMPLE
+.\Test-ServiceEndpoint -ServiceEndpointId <ServiceEndpointId> -ServiceEndpointRequestBody <ServiceEndpointRequestBody> -ProjectName <ProjectName> OrgnizationUri <OrgnizationUri>
+#> 
+Function Test-ServiceEndpoint() {
+    [CmdletBinding()]
+    Param(
+        [ValidateNotNullOrEmpty()]
+        [Parameter(ValueFromPipeline = $true)]
+        [Object]$ServiceEndpointId,
+        [Parameter(Mandatory)]
+        [string]$ServiceEndpointRequestBody,
+        [Parameter(Mandatory)]
+        [string]$ProjectName,
+        [Parameter(Mandatory)]
+        [string]$OrgnizationUri
+    )
+
+    begin {
+        [string]$functionName = $MyInvocation.MyCommand    
+        Write-Debug "${functionName}:Entered" 
+        Write-Debug "${functionName}:ServiceEndpointId=$ServiceEndpointId"
+        Write-Debug "${functionName}:ProjectName=$ProjectName"
+        Write-Debug "${functionName}:OrgnizationUri=$OrgnizationUri"     
+    }
+
+    process {            
+        $headers = Get-DefaultHeadersWithAccessToken -PatToken $env:SYSTEM_ACCESSTOKEN
+
+        $serviceEndpointProxyRequestBody = Initialize-ProxyRequestBody -ServiceEndpointRequestBody $ServiceEndpointRequestBody
+        
+        [string]$endpointProxyServiceEndpointUri = "$($OrgnizationUri)/$ProjectName/_apis/serviceendpoint/endpointproxy?endpointId=$($ServiceEndpointId)&api-version=7.0"
+        Write-Debug "Verifying ServiceEndpoint $($ServiceEndpointId). Put url = $($endpointProxyServiceEndpointUri)"
+        $response = Invoke-RestMethod -Uri $endpointProxyServiceEndpointUri -Headers $headers -Method Post -Body $serviceEndpointProxyRequestBody
+        if ($response.StatusCode -ne [system.net.httpstatuscode]::ok) {
+            throw "Error Verifying serviceEndpoint $($ServiceEndpointId). ErrorMessage = $($response.errorMessage)"
+        }
+        Write-Debug "$($ServiceEndpointId) Verified. Service Endpoint Id = $($ServiceEndpointId)"
+    }
+
+    end {
+        Write-Debug "${functionName}:Exited"
+    }    
+}
