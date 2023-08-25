b48dfb4fb680f29149105fdc8976871ae073d991
Asad Arif <asad.arif@kainos.com>
2023-08-25 13:48:02 +0100

    Link workspace to grafana using PS script

---
+++ scripts/Connect-WorkspaceToGrafana.ps1
@@ -0,0 +1,169 @@
+<#
+.SYNOPSIS
+Link Azure Monitor Workspace to Grafana Dashboard
+
+.DESCRIPTION
+Link Azure Monitor Workspace to Shared Grafana Dashboard in the SSV subscriptions
+
+.PARAMETER ResourceGroupName
+Mandatory. Resource Group Name.
+
+.PARAMETER GrafanaName
+Mandatory. Grafana Dashboard name.
+
+.PARAMETER WorkspaceResourceId
+Mandatory. Azure Monitor Workspace ResourceId.
+
+.EXAMPLE
+.\Connect-WorkspaceToGrafana.ps1 -ResourceGroupName <ResourceGroupName> -GrafanaName <GrafanaName> -WorkspaceResourceId <WorkspaceResourceId>
+#> 
+
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory)] 
+    [string] $ResourceGroupName,
+    [Parameter(Mandatory)]
+    [string] $GrafanaName,
+    [Parameter(Mandatory)]
+    [string] $WorkspaceResourceId
+)
+
+#$ResourceGroupName = 'SSVCDOINFRG3401'
+#$GrafanaName = 'SSVCDOINFGR3401'
+#$WorkspaceResourceId = '/subscriptions/916afc11-9a78-4f8a-91c4-d50754b76733/resourceGroups/SNDCDOINFRG2401/providers/Microsoft.Monitor/accounts/SNDCDOINFMW2401'
+
+Set-StrictMode -Version 3.0
+
+[string]$functionName = $MyInvocation.MyCommand
+[datetime]$startTime = [datetime]::UtcNow
+
+[int]$exitCode = -1
+[bool]$setHostExitCode = (Test-Path -Path ENV:TF_BUILD) -and ($ENV:TF_BUILD -eq "true")
+[bool]$enableDebug = (Test-Path -Path ENV:SYSTEM_DEBUG) -and ($ENV:SYSTEM_DEBUG -eq "true")
+
+Set-Variable -Name ErrorActionPreference -Value Continue -scope global
+Set-Variable -Name InformationPreference -Value Continue -Scope global
+
+if ($enableDebug) {
+    Set-Variable -Name VerbosePreference -Value Continue -Scope global
+    Set-Variable -Name DebugPreference -Value Continue -Scope global
+}
+
+Write-Host "${functionName} started at $($startTime.ToString('u'))"
+Write-Debug "${functionName}:ResourceGroupName=$ResourceGroupName"
+Write-Debug "${functionName}:GrafanaName=$GrafanaName"
+Write-Debug "${functionName}:WorkspaceResourceId=$WorkspaceResourceId"
+
+try {
+    Write-Host "${functionName}:Getting Grafana Dashboard $GrafanaName from Resource Group $ResourceGroupName..."
+    [object]$grafana = Get-AzGrafana -ResourceGroupName $ResourceGroupName -GrafanaName $GrafanaName
+    Write-Host "${functionName}:Finished getting Grafana Dashboard"
+
+    [array]$linkedWorkspaces = $grafana.GrafanaIntegrationAzureMonitorWorkspaceIntegration
+    Write-Debug "${functionName}:linkedWorkspaces=$linkedWorkspaces"
+
+    [string]$workspaceAlreadyLinked = $linkedWorkspaces -Match "$WorkspaceResourceId"
+
+    if ([string]::IsNullOrEmpty($workspaceAlreadyLinked)) {
+        [object]$azureMonitorWorkspaceIntegrationObject = New-AzGrafanaMonitorWorkspaceIntegrationObject -AzureMonitorWorkspaceResourceId $WorkspaceResourceId
+
+        $linkedWorkspaces += $azureMonitorWorkspaceIntegrationObject
+
+        Write-Host "${functionName}:Linking Grafana Dashboard $GrafanaName to Workspace $WorkspaceResourceId..."
+        Update-AzGrafana -ResourceGroupName SSVCDOINFRG3401 -GrafanaName SSVCDOINFGR3401 -MonitorWorkspaceIntegration $linkedWorkspaces -ErrorAction SilentlyContinue
+        Write-Host "${functionName}:Finished linking Grafana Dashboard $GrafanaName to Workspace $WorkspaceResourceId..."
+    }
+}
+catch {
+    $exitCode = -2
+    Write-Error $_.Exception.ToString()
+    throw $_.Exception
+}
+finally {
+    [DateTime]$endTime = [DateTime]::UtcNow
+    [Timespan]$duration = $endTime.Subtract($startTime)
+
+    Write-Host "${functionName} finished at $($endTime.ToString('u')) (duration $($duration -f 'g')) with exit code $exitCode"
+    if ($setHostExitCode) {
+        Write-Debug "${functionName}:Setting host exit code"
+        $host.SetShouldExit($exitCode)
+    }
+    exit $exitCode
+}
+
+<#
+$grafana.GrafanaIntegrationAzureMonitorWorkspaceIntegration
+
+$grafana.GrafanaIntegrationAzureMonitorWorkspaceIntegration.Contains("/subscriptions/55f3b8c6-6800-41c7-a40d-2adb5e4e1bd1/resourceGroups/aatestrg/providers/Microsoft.Monitor/accounts/aaazmonworkspace")
+
+$azureMonitorWorkspaceIntegrationObject = @{
+        AzureMonitorWorkspaceResourceId = "/subscriptions/55f3b8c6-6800-41c7-a40d-2adb5e4e1bd1/resourceGroups/aatestrg/providers/Microsoft.Monitor/accounts/aaazmonworkspace"
+    }
+
+$azureMonitorWorkspaceResourceId = New-AzGrafanaMonitorWorkspaceIntegrationObject -AzureMonitorWorkspaceResourceId "/subscriptions/55f3b8c6-6800-41c7-a40d-2adb5e4e1bd1/resourceGroups/aatestrg/providers/Microsoft.Monitor/accounts/aaazmonworkspace"
+Update-AzGrafana -ResourceGroupName SSVCDOINFRG3401 -GrafanaName SSVCDOINFGR3401 -MonitorWorkspaceIntegration $azureMonitorWorkspaceResourceId
+
+$azureMonitorWorkspaceResourceId = @{
+    AzureMonitorWorkspaceResourceId = "/subscriptions/55f3b8c6-6800-41c7-a40d-2adb5e4e1bd1/resourceGroups/aatestrg/providers/Microsoft.Monitor/accounts/aaazmonworkspace"
+}
+
+$grafana.GrafanaIntegrationAzureMonitorWorkspaceIntegration += $azureMonitorWorkspaceResourceId
+
+
+
+$azureMonitorWorkspaceIntegrationObject = @{
+    AzureMonitorWorkspaceResourceId = "/subscriptions/55f3b8c6-6800-41c7-a40d-2adb5e4e1bd1/resourceGroups/aatestrg/providers/Microsoft.Monitor/accounts/aaazmonworkspace"
+}
+Update-AzGrafana -ResourceGroupName SSVCDOINFRG3401 -GrafanaName SSVCDOINFGR3401 -MonitorWorkspaceIntegration $azureMonitorWorkspaceIntegrationObject -ErrorAction SilentlyContinue
+
+
+#>
+
+
+
+
+
+
+
+
+
+
+
+
+#$grafana.GrafanaIntegrationAzureMonitorWorkspaceIntegration += $resourceId
+
+Update-AzGrafana -ResourceGroupName SSVCDOINFRG3401 -GrafanaName SSVCDOINFGR3401 -MonitorWorkspaceIntegration $azureMonitorWorkspaceResourceId
+
+$azureMonitorWorkspaceResouceIds += [hashtable]$azureMonitorWorkspaceResourceId
+
+$azureMonitorWorkspaceResouceIds += [Microsoft.Azure.PowerShell.Cmdlets.Dashboard.Models.Api20220801.AzureMonitorWorkspaceIntegration]$azureMonitorWorkspaceResourceId
+
+$azureMonitorWorkspaceResourceId.GetType()
+
+#$azureMonitorWorkspaceResourceId = New-AzGrafanaMonitorWorkspaceIntegrationObject -AzureMonitorWorkspaceResourceId "/subscriptions/55f3b8c6-6800-41c7-a40d-2adb5e4e1bd1/resourceGroups/aatestrg/providers/Microsoft.Monitor/accounts/aaazmonworkspace"
+$azureMonitorWorkspaceResourceId = New-AzGrafanaMonitorWorkspaceIntegrationObject -AzureMonitorWorkspaceResourceId "/subscriptions/916afc11-9a78-4f8a-91c4-d50754b76733/resourceGroups/SNDCDOINFRG2401/providers/Microsoft.Monitor/accounts/SNDCDOINFMW2401"
+
+$grafana.GrafanaIntegrationAzureMonitorWorkspaceIntegration += $azureMonitorWorkspaceResourceId
+
+$azureMonitorWorkspaceResouceIds = $grafana.GrafanaIntegrationAzureMonitorWorkspaceIntegration
+$azureMonitorWorkspaceResouceIds += $azureMonitorWorkspaceResourceId
+
+Update-AzGrafana -ResourceGroupName SSVCDOINFRG3401 -GrafanaName SSVCDOINFGR3401 -MonitorWorkspaceIntegration $grafana.GrafanaIntegrationAzureMonitorWorkspaceIntegration
+
+Update-AzGrafana -ResourceGroupName SSVCDOINFRG3401 -GrafanaName SSVCDOINFGR3401 -MonitorWorkspaceIntegration $azureMonitorWorkspaceResourceId
+
+$azureMonitorWorkspaceResourceId = "/subscriptions/55f3b8c6-6800-41c7-a40d-2adb5e4e1bd1/resourceGroups/aatestrg/providers/Microsoft.Monitor/accounts/aaazmonworkspace"
+
+$grafana.GrafanaIntegrationAzureMonitorWorkspaceIntegration += $azureMonitorWorkspaceResourceId
+
+$azureMonitorWorkspaceResouceIds = @($azureMonitorWorkspaceResourceId)
+
+$AzGrafanaMonitorWorkspaceIntegrationObject = New-AzGrafanaMonitorWorkspaceIntegrationObject -AzureMonitorWorkspaceResourceId "/subscriptions/916afc11-9a78-4f8a-91c4-d50754b76733/resourceGroups/SNDCDOINFRG2401/providers/Microsoft.Monitor/accounts/SNDCDOINFMW2401"
+
+$azureMonitorWorkspaceResourceId = New-AzGrafanaMonitorWorkspaceIntegrationObject -AzureMonitorWorkspaceResourceId "/subscriptions/916afc11-9a78-4f8a-91c4-d50754b76733/resourceGroups/SNDCDOINFRG2401/providers/Microsoft.Monitor/accounts/SNDCDOINFMW2401"
+
+Update-AzGrafana -ResourceGroupName SSVCDOINFRG3401 -GrafanaName SSVCDOINFGR3401 -MonitorWorkspaceIntegration $AzGrafanaMonitorWorkspaceIntegrationObject
+Update-AzGrafana -ResourceGroupName SSVCDOINFRG3401 -GrafanaName SSVCDOINFGR3401 -MonitorWorkspaceIntegration $azureMonitorWorkspaceResourceId
+
+
+/subscriptions/916afc11-9a78-4f8a-91c4-d50754b76733/resourceGroups/SNDCDOINFRG2401/providers/Microsoft.Monitor/accounts/SNDCDOINFMW2401
