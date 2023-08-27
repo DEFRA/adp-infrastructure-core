fd6cc28ced0eebb173aa0cb83a80284d44092d34
Asad Arif <asad.arif@kainos.com>
2023-08-27 15:25:58 +0100

    Add $exitCode = 0

--- scripts/Connect-WorkspaceToGrafana.ps1
+++ scripts/Connect-WorkspaceToGrafana.ps1
@@ -28,27 +28,27 @@ param(
     [string] $WorkspaceResourceId
 )
 
-#Set-StrictMode -Version 3.0
-#
-#[string]$functionName = $MyInvocation.MyCommand
-#[datetime]$startTime = [datetime]::UtcNow
-#
-#[int]$exitCode = -1
-#[bool]$setHostExitCode = (Test-Path -Path ENV:TF_BUILD) -and ($ENV:TF_BUILD -eq "true")
-#[bool]$enableDebug = (Test-Path -Path ENV:SYSTEM_DEBUG) -and ($ENV:SYSTEM_DEBUG -eq "true")
-#
-#Set-Variable -Name ErrorActionPreference -Value Continue -scope global
-#Set-Variable -Name InformationPreference -Value Continue -Scope global
-#
-#if ($enableDebug) {
-#    Set-Variable -Name VerbosePreference -Value Continue -Scope global
-#    Set-Variable -Name DebugPreference -Value Continue -Scope global
-#}
-#
-#Write-Host "${functionName} started at $($startTime.ToString('u'))"
-#Write-Debug "${functionName}:ResourceGroupName=$ResourceGroupName"
-#Write-Debug "${functionName}:GrafanaName=$GrafanaName"
-#Write-Debug "${functionName}:WorkspaceResourceId=$WorkspaceResourceId"
+Set-StrictMode -Version 3.0
+
+[string]$functionName = $MyInvocation.MyCommand
+[datetime]$startTime = [datetime]::UtcNow
+
+[int]$exitCode = -1
+[bool]$setHostExitCode = (Test-Path -Path ENV:TF_BUILD) -and ($ENV:TF_BUILD -eq "true")
+[bool]$enableDebug = (Test-Path -Path ENV:SYSTEM_DEBUG) -and ($ENV:SYSTEM_DEBUG -eq "true")
+
+Set-Variable -Name ErrorActionPreference -Value Continue -scope global
+Set-Variable -Name InformationPreference -Value Continue -Scope global
+
+if ($enableDebug) {
+    Set-Variable -Name VerbosePreference -Value Continue -Scope global
+    Set-Variable -Name DebugPreference -Value Continue -Scope global
+}
+
+Write-Host "${functionName} started at $($startTime.ToString('u'))"
+Write-Debug "${functionName}:ResourceGroupName=$ResourceGroupName"
+Write-Debug "${functionName}:GrafanaName=$GrafanaName"
+Write-Debug "${functionName}:WorkspaceResourceId=$WorkspaceResourceId"
 
 try {
     if (-not (Get-Module -ListAvailable -Name 'Az.Dashboard')) {
