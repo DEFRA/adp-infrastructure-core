6ab6e000ac5c946541f465d1d4dac563200ccb09
Ajay Soni <ajay.b.soni@capgemini.com>
2023-10-26 17:45:34 +0100

    Add config data script.

--- infra/core/app-configuration/configuration-store.bicepparam
+++ infra/core/app-configuration/configuration-store.bicepparam
@@ -21,61 +21,61 @@ param environment = '#{{ environment }}'
 
 param principalId = az.getSecret('#{{ ssv3subscriptionId }}', '#{{ ssvSharedResourceGroup }}', '#{{ ssvPlatformKeyVaultName }}', '#{{ tier2ApplicationSPObjectIdSecretName }}')
 
-param keyValues = [
-  {
-    name: 'ENVIRONMENT$Platform'
-    value: '#{{ lower(environment) }}'
-  }
-  {
-    name: 'ACR_NAME$Platform'
-    value: '#{{ infraResourceNamePrefix }}#{{ nc_resource_containerregistry }}#{{ nc_instance_regionid }}01'
-  }
-  {
-    name: 'NAMESPACE$Platform'
-    value: 'flux-config'
-  }
-  {
-    name: 'SERVICEBUS_RG$Platform'
-    value: 'flux-config'
-  }
-  {
-    name: 'SERVICEBUS_RG$Platform'
-    value: '#{{ servicesResourceGroup }}'
-  }
-  {
-    name: 'SERVICEBUS_NS$Platform'
-    value: '#{{ infraResourceNamePrefix }}#{{ nc_resource_servicebus }}#{{ nc_instance_regionid }}01'
-  }
-  {
-    name: 'POSTGRES_SERVER_RG$Platform'
-    value: '#{{ dbsResourceGroup }}'
-  }
-  {
-    name: 'POSTGRES_SERVER$Platform'
-    value: '#{{ dbsResourceNamePrefix }}#{{ nc_resource_postgresql }}#{{ nc_instance_regionid }}01'
-  }
-  {
-    name: 'INFRA_RG$Platform'
-    value: '#{{ servicesResourceGroup }}'
-  }
-  {
-    name: 'TEAM_MI_PREFIX$Platform'
-    value: '#{{ infraResourceNamePrefix }}#{{ nc_resource_managedidentity }}#{{ nc_instance_regionid }}01'
-  }
-  {
-    name: 'TENANT_ID$Platform'
-    value: '#{{ tenantId }}'
-  }
-  {
-    name: 'SUBSCRIPTION_ID$Platform'
-    value: '#{{ subscriptionId }}'
-  }
-  {
-    name: 'SUBSCRIPTION_NAME$Platform'
-    value: '#{{ subscriptionName }}'
-  }
-  {
-    name: 'CLUSTER$Platform'
-    value: '0#{{ environmentId }}'
-  }
-]
+// param keyValues = [
+//   {
+//     name: 'ENVIRONMENT$Platform'
+//     value: '#{{ lower(environment) }}'
+//   }
+//   {
+//     name: 'ACR_NAME$Platform'
+//     value: '#{{ infraResourceNamePrefix }}#{{ nc_resource_containerregistry }}#{{ nc_instance_regionid }}01'
+//   }
+//   {
+//     name: 'NAMESPACE$Platform'
+//     value: 'flux-config'
+//   }
+//   {
+//     name: 'SERVICEBUS_RG$Platform'
+//     value: 'flux-config'
+//   }
+//   {
+//     name: 'SERVICEBUS_RG$Platform'
+//     value: '#{{ servicesResourceGroup }}'
+//   }
+//   {
+//     name: 'SERVICEBUS_NS$Platform'
+//     value: '#{{ infraResourceNamePrefix }}#{{ nc_resource_servicebus }}#{{ nc_instance_regionid }}01'
+//   }
+//   {
+//     name: 'POSTGRES_SERVER_RG$Platform'
+//     value: '#{{ dbsResourceGroup }}'
+//   }
+//   {
+//     name: 'POSTGRES_SERVER$Platform'
+//     value: '#{{ dbsResourceNamePrefix }}#{{ nc_resource_postgresql }}#{{ nc_instance_regionid }}01'
+//   }
+//   {
+//     name: 'INFRA_RG$Platform'
+//     value: '#{{ servicesResourceGroup }}'
+//   }
+//   {
+//     name: 'TEAM_MI_PREFIX$Platform'
+//     value: '#{{ infraResourceNamePrefix }}#{{ nc_resource_managedidentity }}#{{ nc_instance_regionid }}01'
+//   }
+//   {
+//     name: 'TENANT_ID$Platform'
+//     value: '#{{ tenantId }}'
+//   }
+//   {
+//     name: 'SUBSCRIPTION_ID$Platform'
+//     value: '#{{ subscriptionId }}'
+//   }
+//   {
+//     name: 'SUBSCRIPTION_NAME$Platform'
+//     value: '#{{ subscriptionName }}'
+//   }
+//   {
+//     name: 'CLUSTER$Platform'
+//     value: '0#{{ environmentId }}'
+//   }
+// ]
---
+++ infra/core/scripts/Create-ConfigData.ps1
@@ -0,0 +1,92 @@
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory)]
+    [string] $ServicePrincipalId,
+    [Parameter(Mandatory)]
+    [string] $ServicePrincipalKey,
+    [Parameter(Mandatory)]
+    [string] $AzureSubscription,
+    [Parameter(Mandatory)]
+    [string] $AppConfigName,
+    [Parameter(Mandatory)]
+    [string] $ConfigDataFilePath,
+    [Parameter()]
+    [string]$WorkingDirectory = $PWD
+)
+
+Set-StrictMode -Version 3.0
+
+[string]$functionName = $MyInvocation.MyCommand
+[datetime]$startTime = [datetime]::UtcNow
+
+[int]$exitCode = -1
+[bool]$setHostExitCode = (Test-Path -Path ENV:TF_BUILD) -and ($ENV:TF_BUILD -eq "true")
+[bool]$enableDebug = (Test-Path -Path ENV:SYSTEM_DEBUG) -and ($ENV:SYSTEM_DEBUG -eq "true")
+
+Set-Variable -Name ErrorActionPreference -Value Continue -scope global
+Set-Variable -Name InformationPreference -Value Continue -Scope global
+
+if ($enableDebug) {
+    Set-Variable -Name VerbosePreference -Value Continue -Scope global
+    Set-Variable -Name DebugPreference -Value Continue -Scope global
+}
+
+Write-Host "${functionName} started at $($startTime.ToString('u'))"
+Write-Debug "${functionName}:ServicePrincipalId=$ServicePrincipalId"
+Write-Debug "${functionName}:AzureSubscription=$AzureSubscription"
+Write-Debug "${functionName}:AppConfigName=$AppConfigName"
+Write-Debug "${functionName}:ConfigDataFilePath=$ConfigDataFilePath"
+Write-Debug "${functionName}:WorkingDirectory=$WorkingDirectory"
+
+try {
+
+    # Install-Module -Name "Az.Accounts" -RequiredVersion "2.2.3" -Force -SkipPublisherCheck -AllowClobber
+    # Install-Module -Name "Az.KeyVault" -RequiredVersion "4.10.2" -Force -SkipPublisherCheck -AllowClobber
+
+    [System.IO.DirectoryInfo]$moduleDir = Join-Path -Path $WorkingDirectory -ChildPath "scripts/modules/ps-helpers"
+    Write-Debug "${functionName}:moduleDir.FullName=$($moduleDir.FullName)"
+    Import-Module $moduleDir.FullName -Force
+
+    Write-Host "${functionName}:Connecting to Azure..."
+    [SecureString]$SecuredPassword = ConvertTo-SecureString -AsPlainText -String $ServicePrincipalKey
+    [PSCredential]$Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $ServicePrincipalId, $SecuredPassword
+    $null = Connect-AzAccount -ServicePrincipal -TenantId $env:tenantId -Credential $Credential
+    $null = Set-AzContext -Subscription $AzureSubscription
+    Write-Host "${functionName}:Connected to Azure and set context to '$AzureSubscription'"
+    
+    # Get-Image -KeyVaultName $KeyVaultName -NGINXCertSecretName $NGINXCertSecretName -NGINXKeySecretName $NGINXKeySecretName -NGINXVersion $NGINXVersion
+
+    # Publish-Image -AcrName $AcrName.ToLower() -NGINXVersion $NGINXVersion
+
+    $settings = Get-Content $(Join-Path -Path $WorkingDirectory -ChildPath $ConfigDataFilePath)
+
+    $settings | ConvertFrom-Json | ForEach-Object {
+        az appconfig kv set `
+            --name $AppConfigName `
+            --key $setting.key `
+            --value $setting.value `
+            --content-type $setting.contentType `
+            --label $setting.label
+        # --yes | Out-Null
+    }
+
+    # az appconfig kv set --name $appConfigName --key $newKey --value "Value 1"
+
+    $exitCode = 0
+}
+catch {
+    $exitCode = -2
+    Write-Error $_.Exception.ToString()
+    throw $_.Exception
+}
+finally {
+    [DateTime]$endTime = [DateTime]::UtcNow
+    [Timespan]$duration = $endTime.Subtract($startTime)
+
+    Write-Host "${functionName} finished at $($endTime.ToString('u')) (duration $($duration -f 'g')) with exit code $exitCode"
+    if ($setHostExitCode) {
+        Write-Debug "${functionName}:Setting host exit code"
+        $host.SetShouldExit($exitCode)
+    }
+    exit $exitCode
+}
