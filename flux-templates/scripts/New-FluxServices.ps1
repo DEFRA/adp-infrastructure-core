796199efe239095601606737395791ea6f810acf
Asad Arif <asad.arif@capgemini.com>
2023-12-13 08:51:06 +0000

    Set version to 1.0.1

--- flux-templates/scripts/New-FluxServices.ps1
+++ flux-templates/scripts/New-FluxServices.ps1
@@ -66,18 +66,12 @@ function New-Directory {
     }
 }
 
-# $TemplatesPath = 'C:\Users\asaarif\GitRepos\adp-flux-services-onboarding-aa\templates'
-# $FluxServicesPath = 'C:\Users\asaarif\GitRepos\adp-flux-services-1\services'
-
 [hashtable]$programmeDetails = $OnBoardingManifest | ConvertFrom-Json -AsHashtable
-# [PSCustomObject]$programmeDetails = Get-Content -Raw -Path $OnBoardingManifest | ConvertFrom-Json -AsHashtable
 
 $programmeName = $programmeDetails.name
 $serviceCode = $programmeDetails.servicecode
 $services = $programmeDetails.services
 $environments = $programmeDetails.environments
-$programmePath = "$FluxServicesPath\$programmeName"
-$programmeBaseDirectory = "$programmePath\base"
 
 $lookupTable = @{
     '__PROGRAMME_NAME__' = $programmeName
@@ -88,28 +82,25 @@ $lookupTable = @{
     '__DEPENDS_ON__'     = 'infra'
 }
 
-# # Create programme directory
-# New-Item -Path $programmePath -Type Directory -Force # Should we use force
-
-# # Create base directory
-# New-Item -Path $programmeBaseDirectory -Type Directory -Force # Should we use force
+$programmePath = "$FluxServicesPath\$programmeName"
+$programmeBaseDirectory = "$programmePath\base"
 
 New-Directory -DirectoryPath $programmePath
 New-Directory -DirectoryPath $programmeBaseDirectory
 
-#TEMPLATE PATHS
 $templateProgrammePath = "$TemplatesPath/programme"
 $templateProgrammeBasePath = "$templateProgrammePath/base"
 $templateProgrammeServicePath = "$templateProgrammePath/service"
 $templateProgrammeEnvironmentPath = "$templateProgrammePath/environment"
 
+# CREATE BASE DIRECTORIES AND FILES
 New-Directory -DirectoryPath "$programmeBaseDirectory/patch"
 Copy-Item -Path "$templateProgrammeBasePath/kustomization.yaml" -Destination "$programmeBaseDirectory/kustomization.yaml"
 Copy-Item -Path "$templateProgrammeBasePath/patch/kustomization.yaml" -Destination "$programmeBaseDirectory/patch/kustomization.yaml"
 ReplaceTokens -TemplateFile "$templateProgrammeBasePath/patch/kustomize.yaml" -DestinationFile "$programmeBaseDirectory/patch/kustomize.yaml"
 ReplaceTokens -TemplateFile "$templateProgrammeBasePath/helm-repository.yaml" -DestinationFile "$programmeBaseDirectory/helm-repository.yaml"
 
-# Create services directory
+# CREATE SERVICES DIRECTORIES AND FILES
 foreach ($service in $services) {
     New-Directory -DirectoryPath "$programmePath/$($service.name)/deploy/base"
     Copy-Item -Path "$templateProgrammeServicePath/kustomization.yaml" -Destination $programmePath/$($service.name)/kustomization.yaml
@@ -172,6 +163,7 @@ foreach ($service in $services) {
     }
 }
 
+# CREATE ENVIRONMENT DIRECTORIES AND FILES
 foreach ($environment in $environments) {
     foreach ($instance in $environment.instances) {
         New-Directory -DirectoryPath "$programmePath/$($environment.name)/0$instance"
@@ -183,6 +175,7 @@ foreach ($environment in $environments) {
     }
 }
 
+# CREATE FEATURE BRANCH
 [string]$userEmail = "ado@noemail.com"
 [string]$userName = "Devops"
 
@@ -194,14 +187,10 @@ git pull origin main
 
 git checkout -b "feature-$($programmeName)-onboarding"
 
-# git pull origin main
-
 git add -A
 
-[string]$commitMessage = "Add new version flux project"
+[string]$commitMessage = "Add Flux Services for $($programmeName)"
 [string]$author = "ADO Devops <ado@noemail.com>"
 git commit -am $commitMessage --author=$author
-
-git status
 
 git push --set-upstream origin "feature-$($programmeName)-onboarding"
