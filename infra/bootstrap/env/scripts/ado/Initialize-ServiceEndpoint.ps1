0d3439dd95041f6f8ee603cca92671a45ddf2d0a
santosh-gh <57346552+santosh-gh@users.noreply.github.com>
2024-07-26 17:37:28 +0100

    update

--- infra/bootstrap/env/scripts/ado/Initialize-ServiceEndpoint.ps1
+++ infra/bootstrap/env/scripts/ado/Initialize-ServiceEndpoint.ps1
@@ -61,6 +61,43 @@ try {
    
     $env:AZURE_DEVOPS_EXT_PAT = $env:SYSTEM_ACCESSTOKEN 
     az devops configure --defaults organization=$devopsOrgnizationUri project=$devopsProjectName
+
+    # Define Azure DevOps variables
+    $spiname = "ADO-DefraGovUK-ADP-SND1-ContUAA"
+    $appId = "ea14266a-4d9e-4674-9f98-08d077ac8d93"
+    $subscriptionId = "55f3b8c6-6800-41c7-a40d-2adb5e4e1bd1"
+    $subsName = "AZD-ADP-SND1"
+    $tenantID = "6f504113-6b64-43f2-ade9-242e05780007"
+    $organization = "defragovuk"
+    $project = "DEFRA-FFC"
+    $serviceConnectionName = "test"
+
+    # Define the service connection configuration
+    $serviceConnectionConfig = @{
+        "name" = $serviceConnectionName
+        "type" = "azuresp"
+        "url" = "https://management.azure.com/"
+        "authorization" = @{
+            "parameters" = @{
+                "tenantid" = $tenantId
+                "serviceprincipalid" = $appId
+                "authenticationType" = "spnKey"
+            }
+            "scheme" = "ServicePrincipal"
+        }
+        "data" = @{
+            "subscriptionId" = $subscriptionId
+            "subscriptionName" = $subsName
+        }
+    }
+
+    # Convert configuration to JSON
+    $serviceConnectionConfigJson = $serviceConnectionConfig | ConvertTo-Json -Depth 10
+
+    # Create the service connection
+    az devops service-endpoint create --organization https://dev.azure.com/$organization --project $project --service-endpoint-configuration $serviceConnectionConfigJson
+
+
     
     if ($LASTEXITCODE -ne 0) {
         throw "Error configuring default devops organization=$devopsOrgnizationUri project=$devopsProjectName with exit code $LASTEXITCODE"
