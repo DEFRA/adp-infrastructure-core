d3c3e0622ec312242e22f7f92266582d4c2f27e1
santosh-gh <57346552+santosh-gh@users.noreply.github.com>
2024-07-31 16:33:24 +0100

    update

--- infra/bootstrap/env/scripts/ado/Initialize-ServiceEndpoint.ps1
+++ infra/bootstrap/env/scripts/ado/Initialize-ServiceEndpoint.ps1
@@ -8,9 +8,6 @@ Create an Azure RM type service endpoint (ServiceConnection). It also verifies t
 .PARAMETER ServiceEndpointJsonPath
 Mandatory. Service connection configuration file.
 
-.PARAMETER FederatedEndpointJsonPath
-Mandatory. Connection configuration file.
-
 .PARAMETER WorkingDirectory
 Optional. Working directory. Default is $PWD.
 
@@ -27,7 +24,6 @@ param(
     [Parameter()]
     [string]$WorkingDirectory = $PWD
 )
-
 
 Set-StrictMode -Version 3.0
 
@@ -51,80 +47,38 @@ Write-Debug "${functionName}:ServiceEndpointJsonPath=$ServiceEndpointJsonPath"
 Write-Debug "${functionName}:FederatedEndpointJsonPath=$FederatedEndpointJsonPath"
 Write-Debug "${functionName}:WorkingDirectory=$WorkingDirectory"
 
-[System.IO.DirectoryInfo]$moduleDir = Join-Path -Path $WorkingDirectory -ChildPath "scripts/modules/ado"
-Write-Debug "${functionName}:moduleDir.FullName=$($moduleDir.FullName)"
+try {
 
-Import-Module $moduleDir.FullName -Force
+    [System.IO.DirectoryInfo]$moduleDir = Join-Path -Path $WorkingDirectory -ChildPath "scripts/modules/ado"
+    Write-Debug "${functionName}:moduleDir.FullName=$($moduleDir.FullName)"
 
-# Initialize az devops commands
-[string]$devopsOrgnizationUri = $env:SYSTEM_TEAMFOUNDATIONCOLLECTIONURI
-[string]$devopsProjectName = $env:SYSTEM_TEAMPROJECT
-[string]$devopsProjectId = $env:SYSTEM_TEAMPROJECTID
-Write-Debug "${functionName}:devopsOrgnizationUri=$devopsOrgnizationUri"
-Write-Debug "${functionName}:devopsProjectName=$devopsProjectName"
-Write-Debug "${functionName}:devopsProjectId=$devopsProjectId"
+    Import-Module $moduleDir.FullName -Force
 
-$env:AZURE_DEVOPS_EXT_PAT = $env:SYSTEM_ACCESSTOKEN
-
-az devops configure --defaults organization=$devopsOrgnizationUri project=$devopsProjectName    
-
-if ($LASTEXITCODE -ne 0) {
-    throw "Error configuring default devops organization=$devopsOrgnizationUri project=$devopsProjectName with exit code $LASTEXITCODE"
-}
-
-[PSCustomObject]$serviceEndpoints = Get-Content -Raw -Path $ServiceEndpointJsonPath | ConvertFrom-Json 
-$principalId = (az ad app list --display-name $serviceEndpoints.azureRMServiceConnections.appRegName | convertFrom-Json).appId
-Write-Host "The principalId is '$principalId'"
-
-Function CreateServiceConnection() {
-    [CmdletBinding(SupportsShouldProcess)]
-    Param(
-        [Parameter(Mandatory)] 
-        [string]$ServiceEndpointJsonPath,
-        [Parameter()]
-        [string]$workingDirectory = $PWD
-    )
-
-    try {        
-
-        $functionInput = @{
-            ProjectId      = $devopsProjectId
-            ProjectName    = $devopsProjectName
-            OrgnizationUri = $devopsOrgnizationUri
-        }
-
-        $serviceEndpoints.azureRMServiceConnections | Set-ServiceEndpoint @functionInput                 
-        
-        $exitCode = 0    
+    # Initialize az devops commands
+    [string]$devopsOrgnizationUri = $env:SYSTEM_TEAMFOUNDATIONCOLLECTIONURI
+    [string]$devopsProjectName = $env:SYSTEM_TEAMPROJECT
+    [string]$devopsProjectId = $env:SYSTEM_TEAMPROJECTID
+    Write-Debug "${functionName}:devopsOrgnizationUri=$devopsOrgnizationUri"
+    Write-Debug "${functionName}:devopsProjectName=$devopsProjectName"
+    Write-Debug "${functionName}:devopsProjectId=$devopsProjectId"
+   
+    $env:AZURE_DEVOPS_EXT_PAT = $env:SYSTEM_ACCESSTOKEN 
+    az devops configure --defaults organization=$devopsOrgnizationUri project=$devopsProjectName
+    if ($LASTEXITCODE -ne 0) {
+        throw "Error configuring default devops organization=$devopsOrgnizationUri project=$devopsProjectName with exit code $LASTEXITCODE"
     }
-    catch {
-        $exitCode = -2
-        Write-Error $_.Exception.ToString()
-        throw $_.Exception
+
+    [PSCustomObject]$serviceEndpoints = Get-Content -Raw -Path $ServiceEndpointJsonPath | ConvertFrom-Json
+
+    $functionInput = @{
+        ProjectId      = $devopsProjectId
+        ProjectName    = $devopsProjectName
+        OrgnizationUri = $devopsOrgnizationUri
     }
-    finally {
-        [DateTime]$endTime = [DateTime]::UtcNow
-        [Timespan]$duration = $endTime.Subtract($startTime)
 
-        Write-Host "${functionName} finished at $($endTime.ToString('u')) (duration $($duration -f 'g')) with exit code $exitCode"
-        if ($setHostExitCode) {
-            Write-Debug "${functionName}:Setting host exit code"
-            $host.SetShouldExit($exitCode)
-        }
-        exit $exitCode
-    }    
-}
+    $serviceEndpoints.azureRMServiceConnections | Set-ServiceEndpoint @functionInput
 
-Function CreateFederatedCredentialServiceConnection() {
-    [CmdletBinding(SupportsShouldProcess)]
-    Param(
-        [Parameter(Mandatory = $true)]
-        [ValidateNotNullOrEmpty()]
-        [string]$FederatedEndpointJsonPath,
-        [Parameter(Mandatory = $false)]
-        [string]$graphApiversion = "v1.0"
-    )
-    
+  
     $federatedServiceEndpoint = Get-Content -Raw -Path $FederatedEndpointJsonPath | ConvertFrom-Json
     $serviceConnectionName = $federatedServiceEndpoint.serviceEndpointProjectReferences[0].name
     Write-Host "Service connection name '$serviceConnectionName'"
@@ -145,8 +99,23 @@ Function CreateFederatedCredentialServiceConnection() {
         $jsonObject | ConvertTo-Json -depth 32| set-content $FederatedEndpointJsonPath        
 
         az devops service-endpoint create --service-endpoint-configuration $FederatedEndpointJsonPath --org $devopsOrgnizationUri --project $devopsProjectName
-    }     
-}
+    }        
 
-CreateServiceConnection -serviceEndpointJsonPath $ServiceEndpointJsonPath 
-CreateFederatedCredentialServiceConnection -federatedEndpointJsonPath $FederatedEndpointJsonPath
+    $exitCode = 0    
+}
+catch {
+    $exitCode = -2
+    Write-Error $_.Exception.ToString()
+    throw $_.Exception
+}
+finally {
+    [DateTime]$endTime = [DateTime]::UtcNow
+    [Timespan]$duration = $endTime.Subtract($startTime)
+
+    Write-Host "${functionName} finished at $($endTime.ToString('u')) (duration $($duration -f 'g')) with exit code $exitCode"
+    if ($setHostExitCode) {
+        Write-Debug "${functionName}:Setting host exit code"
+        $host.SetShouldExit($exitCode)
+    }
+    exit $exitCode
+}
